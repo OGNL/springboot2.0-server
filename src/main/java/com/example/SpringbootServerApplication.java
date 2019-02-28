3e3072a876c70ba791d7b5aafe4ffc924ddca7fb
zhangtao <miss_null@163.com>
2019-02-28 22:44:20 +0800

    1、添加注释

--- src/main/java/com/example/SpringbootServerApplication.java
+++ src/main/java/com/example/SpringbootServerApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@ImportResource("classpath:spring-dubbo.xml")
-@EnableTransactionManagement
-@SpringBootApplication
+@ImportResource("classpath:spring-dubbo.xml") //用来加载xml配置文件
+@EnableTransactionManagement //开启事务支持
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class SpringbootServerApplication {
 
     public static void main(String[] args) {
